2684fbb51a9913773a0d86d45ae8bb8177826a9f
jparadelo <juniorparadelo@gmail.com>
2020-12-19 17:22:19 +0100

    Primer commit desde sourcetree como base para la app

---
+++ siteapp/src/main/java/es/udc/siteapp/SiteappApplication.java
@@ -0,0 +1,13 @@
+package es.udc.siteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteappApplication.class, args);
+	}
+
+}
---
+++ siteapp/src/test/java/es/udc/siteapp/SiteappApplicationTests.java
@@ -0,0 +1,13 @@
+package es.udc.siteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
