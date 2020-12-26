419de59a0650f4b218955b48c031444fcecf4eb6
jparadelo <juniorparadelo@gmail.com>
2020-12-26 16:42:10 +0100

    Permitir el acceso a la API de swagger sin autenticación ni tokens

--- siteapp/src/main/java/es/udc/siteapp/configuration/SecurityConfig.java
+++ siteapp/src/main/java/es/udc/siteapp/configuration/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -69,6 +70,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 		// disable page caching
 		httpSecurity.headers().cacheControl();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
+				"/configuration/security", "/swagger-ui.html", "/webjars/**");
 	}
 
 	@Override
